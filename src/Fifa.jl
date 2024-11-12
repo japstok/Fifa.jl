bcfc9aff884199aa01e18cc704a35c9ba00dd6b1
japstok <kentokado@gmail.com>
2024-11-12 10:57:37 +0100

    structures dans fifa.jl

--- src/Fifa.jl
+++ src/Fifa.jl
@@ -2,10 +2,14 @@ module Fifa
 
 using Vizagrams, Colors, PlutoUI, CSV, DataFrames, Markdown, InteractiveUtils
 
+export Terrain
 export creation_terrain
+export Player
 export adjust_rating
 export find_player_by_name
+export Team
 export calcul_team_score
+export Formation
 export creation_formation
 export player_input
 export find_position_category
